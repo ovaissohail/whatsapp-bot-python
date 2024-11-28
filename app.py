8b616de42b76d60bc16c5a03c364d0cd409a24e7
ovaissohail <ovaissohail008@gmail.com>
2024-11-28 20:34:20 +0500

    Update port

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ def process_message():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
